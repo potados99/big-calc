aaa16b9671c0f435de30537216fc1b5a3bdb0d7f
potados99 <potados99@gmail.com>
2019-01-24 18:57:00 +0900

    Update main.

--- main.c
+++ main.c
@@ -10,17 +10,16 @@
 
 int main(int argc, const char * argv[]) {
     BIGNUM *bn = bn_new();
-    bn_str2bn(bn, "1234");
+    bn_str2bn(bn, "45678765456776543456789876543456789098723456");
     
-    char * bn_in_string = bn_bn2str(bn);
     printf("length of bn is %ld\n", bn_len(bn));
-    printf("content of bn is %s\n", bn_in_string);
-    free(bn_in_string);
+    printf("content of bn is" );
+    bn_print(stdout, bn);
     
     foreach_num(bn, _myd) {
         printf("digit at %zu is %d.\n", _index, _myd);
     }
-    
+    
     return 0;
 }
 
